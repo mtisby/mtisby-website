4b7840dadfb60a03fc3778ed401b8aeda334ba26
mtisby <marieltisby1998@gmail.com>
2021-08-17 23:48:29 -0700

    styling & navbar added; added transitions

--- navigation.js
+++ navigation.js
@@ -1,9 +1,30 @@
 const menuIcon = document.querySelector('#menuImg');
 menuIcon.addEventListener('click', function myFunction() {
-    const navbar = document.querySelector('#navUl');
+    const navbar = document.querySelector('#navigation');
         if (navbar.style.display === "block") {
           navbar.style.display = "none";
         } else {
             navbar.style.display = "block";
         }
-      });
+});
+
+const linkedinNav = document.querySelector('#linkedinNav');
+linkedinNav.addEventListener("click", function(){
+    document.location.href = "https://www.linkedin.com/in/mariel-tisby-5a7a93bb";
+});
+const githubNav = document.querySelector('#githubNav');
+githubNav.addEventListener("click", function(){
+    document.location.href = "https://github.com/mtisby";
+});
+    
+const emailNav = document.querySelector('#emailNav');
+emailNav.addEventListener("click", function(){
+    document.location.href = "./email.html";
+});
+
+const resumeNav = document.querySelector('#resumeNav');
+resumeNav.addEventListener("click", function(){
+    document.location.href = "./email.html";
+});
+    
+    
