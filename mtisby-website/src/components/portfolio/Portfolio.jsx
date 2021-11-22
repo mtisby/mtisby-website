ed2bf413a25bd480a31ab167a012471c0d2f92a3
mtisby <marieltisby1998@gmail.com>
2021-11-21 21:08:38 -0800

    updates

--- mtisby-website/src/components/portfolio/Portfolio.jsx
+++ mtisby-website/src/components/portfolio/Portfolio.jsx
@@ -6,7 +6,8 @@ const Portfolio = () => {
         "cheflavor": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636655757/portfolio/cheflavor_banyqt.gif",
         "js-calendar": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636658371/portfolio/js-calenfdar_auidpm.gif",
         "signum": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636659040/portfolio/signum_oslci3.gif",
-        "cheflavor-staffportal": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636855482/portfolio/cheflavor-staffportal_ijllwa.gif"
+        "cheflavor-staffportal": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636855482/portfolio/cheflavor-staffportal_ijllwa.gif",
+        "poodle-doodle": "https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637555977/paint-app/Poodle_Doodle_-_Google_Chrome_2021-11-21_20-21-49_SparkVideo_1_x0u3jj.gif"
     }
 
     const handleHoverEnter = (e) => {
@@ -44,6 +45,14 @@ const Portfolio = () => {
             </div>
             <h1 className="portfolioHeaderh1">Projects</h1>
             <div className="projects">
+            <div className="projectDiv">
+                    <h1 className="projectHeader">Poodle Doodle</h1>
+                <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1637557563/paint-app/poodle-doodle_gkby0l.png" alt="poodle doodle website gif" id="poodle-doodle"/>
+                    <div className="buttonDiv">
+                        <button className="projectButtons"><a href="https://pacific-bastion-50258.herokuapp.com/">website</a></button>
+                        <button className="projectButtons"><a href="https://github.com/mtisby/paint-app">repository</a></button>
+                    </div>
+                </div>
                 <div className="projectDiv">
                     <h1 className="projectHeader">Cheflavor</h1>
                     <img onMouseEnter={handleHoverEnter} onMouseLeave={ handleHoverLeave } src="https://res.cloudinary.com/dr0ofxgkz/image/upload/v1636656191/portfolio/cheflavor_dzfi1a.png" alt="cheflavor website gif" id="cheflavor"/>
